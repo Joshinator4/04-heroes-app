0bedf768c2400ab7ff00bf2fe465e3ec9bac5125
DAM2 <wowmewet1@gmail.com>
2025-01-27 18:34:50 +0100

    Subiendo cambio en docs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Error404PageComponent } from './shared/pages/error404-page/error404-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(m=>m.AuthModule)//esto añade el módulo por carga perezosa(LazyLoad)
+  },
+  {
+    path: 'heroes',
+    loadChildren: () => import('./heroes/heroes.module').then(m=>m.HeroesModule)//esto añade el módulo por carga perezosa(LazyLoad)
+  },
+  {
+    path: '404',
+    component: Error404PageComponent
+  },
+  {
+    path: '',
+    redirectTo: 'heroes',
+    pathMatch: 'full' //significa que la URL debe coincidir exactamente con la ruta especificada. Si hay una coincidencia parcial, la ruta no se activará.
+  },
+  {
+    path: '**',
+    redirectTo: '404'// (**) por si se introduce una ruta errónea, que redirija a 404
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
