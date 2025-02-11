44fcc58f56bf92d58099e228cf03917bcbc300ec
DAM2 <wowmewet1@gmail.com>
2025-02-11 20:16:17 +0100

    Subiendo cambio en docs

--- src/app/heroes/pages/hero-page/hero-page.component.ts
+++ src/app/heroes/pages/hero-page/hero-page.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { delay, switchMap } from 'rxjs';
+import { Hero } from '../../interfaces/hero.interfaces';
 
 @Component({
   selector: 'app-hero-page',
@@ -6,6 +10,33 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class HeroPageComponent {
+export class HeroPageComponent implements OnInit{
 
+  public hero?: Hero;
+  constructor(
+    private heroesService: HeroesService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ){
+
+  }
+
+  volverAlListado(): void{
+    this.router.navigate(['/heroes/list'])
+  }
+
+  ngOnInit(): void {
+    // console.log(this.activatedRoute.params);esta forma no nos sirve
+
+    //!Si queremos acceder a los datos del id en el que nos encontramos, debemos hacerlo de la segunda forma, ya que params es un Observable, y por tanto necesitamos suscribirnos para obtener el valor de forma asíncrona.
+    this.activatedRoute.params.pipe(
+      switchMap(({id}) => this.heroesService.getHeroById(id))
+    ).subscribe(hero => {
+      if(!hero) return this.router.navigate(['/heroes/list']);
+      this.hero = hero;
+      console.log(hero);
+
+      return;
+    })
+  }
 }
--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { catchError, Observable, of } from 'rxjs';
 import { Hero } from '../interfaces/hero.interfaces';
 import { environments } from 'src/environments/environments';
 
@@ -15,6 +15,11 @@ export class HeroesService {
 
   getHeroes(): Observable<Hero[]>{
     return this.httpClient.get<Hero[]>(`${ this.baseUrl }/heroes`)
+  }
+
+  //!indicamos además un pipe para poder capturar el error en caso de undefined, y tenemos que meterlo dentro de un “of()”, ya que la función devuelve un observable, y un undefined a secas no es un observable. Para convertir de forma rápida un valor en observable, tan sólo tenemos que utilizar un of():
+  getHeroById(id: string): Observable<Hero|undefined>{
+    return this.httpClient.get<Hero>(`${this.baseUrl}/heroes/${id}`).pipe(catchError(error => of(undefined)));
   }
 
 }
