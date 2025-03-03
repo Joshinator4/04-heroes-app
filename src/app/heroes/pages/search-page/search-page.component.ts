e92c68c47db9da7b6d431d8cc9209f9e6340eace
DAM2 <wowmewet1@gmail.com>
2025-03-03 20:47:30 +0100

    Subiendo cambio en docs

--- src/app/heroes/pages/search-page/search-page.component.ts
+++ src/app/heroes/pages/search-page/search-page.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Hero } from '../../interfaces/hero.interfaces';
+import { HeroesService } from '../../services/heroes.service';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 
 @Component({
   selector: 'app-search-page',
@@ -10,10 +12,24 @@ import { Hero } from '../../interfaces/hero.interfaces';
 })
 export class SearchPageComponent {
   public searchInput = new FormControl('');
-  public heores: Hero[] = [];
+  public heroes: Hero[] = [];
   public selectedHero?: Hero;
 
+  constructor(private heroesService: HeroesService){}
   public searchHero(){
-    // this.searchInput.
+    const value: string= this.searchInput.value || '';
+    this.heroesService.getSuggestions(value).subscribe(
+      heroes => this.heroes = heroes
+    );
+  }
+  public onSelectedOption(event: MatAutocompleteSelectedEvent){
+    if(!event.option.value){
+      this.selectedHero= undefined;
+      return;
+
+    }
+    const hero: Hero = event.option.value;
+    this.searchInput.setValue(hero.superhero);
+    this.selectedHero = hero;
   }
 }
